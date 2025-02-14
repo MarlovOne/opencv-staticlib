1d4fdb4ba61834674099168ff72de8d18f5a2bc8
Lovro Markovic <lovro@janeasystems.com>
2025-02-14 19:37:59 +0100

    Add Windows libraries - core,imgproc

---
+++ ARM64/ARM64/vc17/staticlib/OpenCVModules-release.cmake
@@ -0,0 +1,59 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "zlib" for configuration "Release"
+set_property(TARGET zlib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(zlib PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/zlib.lib"
+  )
+
+list(APPEND _cmake_import_check_targets zlib )
+list(APPEND _cmake_import_check_files_for_zlib "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/zlib.lib" )
+
+# Import target "libjpeg-turbo" for configuration "Release"
+set_property(TARGET libjpeg-turbo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libjpeg-turbo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/libjpeg-turbo.lib"
+  )
+
+list(APPEND _cmake_import_check_targets libjpeg-turbo )
+list(APPEND _cmake_import_check_files_for_libjpeg-turbo "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/libjpeg-turbo.lib" )
+
+# Import target "ittnotify" for configuration "Release"
+set_property(TARGET ittnotify APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ittnotify PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/ittnotify.lib"
+  )
+
+list(APPEND _cmake_import_check_targets ittnotify )
+list(APPEND _cmake_import_check_files_for_ittnotify "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/ittnotify.lib" )
+
+# Import target "opencv_core" for configuration "Release"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/opencv_core4110.lib"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_core )
+list(APPEND _cmake_import_check_files_for_opencv_core "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/opencv_core4110.lib" )
+
+# Import target "opencv_imgproc" for configuration "Release"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/opencv_imgproc4110.lib"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgproc )
+list(APPEND _cmake_import_check_files_for_opencv_imgproc "${_IMPORT_PREFIX}/ARM64/vc17/staticlib/opencv_imgproc4110.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
